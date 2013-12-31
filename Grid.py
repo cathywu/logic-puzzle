d80b0749d453e08e9ecdf5af1759cea8d5aaf3f1
Cathy Wu <cathywu@alum.mit.edu>
2013-12-30 22:49:04 -0800

    new grid format!

--- Grid.py
+++ Grid.py
@@ -6,7 +6,8 @@ class Grid:
         self.x = x
         self.y = y
         self.blocks = np.zeros((self.x,self.y))
-        self.walls = np.zeros((self.x+1,self.y+1))
+        self.vwalls = np.zeros((self.x,self.y+1))
+        self.hwalls = np.zeros((self.x+1,self.y))
 
     def add_obstacle(self, x, y):
         self.blocks[x,y] = 1
@@ -14,9 +15,19 @@ class Grid:
     def is_obstacle(self,x,y):
         return self.blocks[x,y]
 
-    def add_wall(self, x, y):
-        self.walls[x,y] = 1
+    def add_vwall(self, x, y):
+        self.vwalls[x,y] = 1
 
-    def is_wall(self,x,y):
-        return self.walls[x,y]
+    def is_vwall(self,x,y):
+        return self.vwalls[x,y]
 
+    def add_hwall(self, x, y):
+        self.vwalls[x,y] = 1
+
+    def is_hwall(self,x,y):
+        return self.hwalls[x,y]
+
+    def draw_grid(self):
+        for i in range(self.y):
+            print self.blocks[i,:]
+            # print ' '.join(['X' if bool(x) is not None else 'yyyy' for x in self.blocks[i,:]])
--- logic-puzzle.py
+++ logic-puzzle.py
@@ -1,3 +1,5 @@
 from Grid import Grid
 
 g = Grid(10,15)
+g.add_obstacle(3,4)
+# g.draw_grid() # FIXME
