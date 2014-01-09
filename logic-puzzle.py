2bb8f8eade4da6dfda99cbe272a9877e74780a8a
Ankur Mehta <mehtank@31-34-189.wireless.csail.mit.edu>
2014-01-09 01:15:26 -0500

    removed old stuff

--- logic-puzzle.py
+++ logic-puzzle.py
@@ -5,21 +5,13 @@ from Thing import *
 import time, sys
 
 g = Grid(6, 9)
-# g.delWall([0,4], NORTH)
-# g.addWallQuad([2,0], EAST)
-# g.addWallQuad([4,0], EAST)
-# g.addWallQuad([2,1], SOUTH)
-# g.addWallQuad([1,2], SOUTH)
-# g.addWallQuad([3,2], SOUTH)
-g.addWall180([2,3], EAST) # wormhole
-g.addWall180([4,3], EAST) # wormhole
-g.addWall180([4,2], EAST) # wormhole
-g.addWall180([3,1], SOUTH) # wormhole
-# g.addWall180([1,1], EAST) # wormhole
-# g.addWall180([4,2], NORTH) # wormhole
-g.addWall180([0,3], SOUTH) # wormhole
-g.addWall180([1,2], SOUTH) # wormhole
-g.addWall180([1,2], WEST) # wormhole
+g.addWall180([2,3], EAST)
+g.addWall180([4,3], EAST)
+g.addWall180([4,2], EAST)
+g.addWall180([3,1], SOUTH)
+g.addWall180([0,3], SOUTH)
+g.addWall180([1,2], SOUTH)
+g.addWall180([1,2], WEST) 
 
 a = Alice()
 m = Mirror()
@@ -27,7 +19,6 @@ c = Chess()
 f = Feather()
 
 a.spawn(g, (5,0))
-# a.turn(BACK)
 m.spawn(g, (1,4))
 # m.spawn(g, (2,5))
 #c.spawn(g, (0,5))
